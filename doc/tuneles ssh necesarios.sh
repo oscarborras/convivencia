179a9abec9fa2bb5a2f5bb315979a774c78b594a
Oscar B. <oscarborras@iesjulioverne.es>
2026-03-14 20:38:24 +0100

    chore: eliminar carpeta doc del tracking (datos sensibles)

--- doc/tuneles ssh necesarios.sh
+++
@@ -1,9 +0,0 @@
-# comando a ejecutar para que funcione la conexión mcp con supabase-local:
-# Requisitos: conexión previa a la VPN del centro
-
-# servidor Supabase desarrollo: 172.20.250.41
-ssh -L localhost:8080:localhost:8000 root@172.20.250.41
-
-
-#Para Google Auth
-ssh -L 0.0.0.0:8000:localhost:8000 root@172.20.250.41
